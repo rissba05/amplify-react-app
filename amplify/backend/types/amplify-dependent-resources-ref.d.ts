9fdfaa11b3f36eff5e9a6cd8fed373f5f295bb81
rissba05 <rissba05@gmail.com>
2022-02-14 12:41:47 -0600

    ran amplify push
    and tested in console by running amplify console api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,17 @@
+export type AmplifyDependentResourcesAttributes = {
+    "function": {
+        "cryptofunction22s": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "cryptoapi22s": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
