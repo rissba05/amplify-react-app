010f0b504bcd87c572e1edf2818d611270ad7166
rissba05 <rissba05@gmail.com>
2022-02-22 10:11:04 -0600

    backend working calling coinlore
    tested in aws console with query string params

--- amplify/backend/function/cryptofunction22s/src/app.js
+++ amplify/backend/function/cryptofunction22s/src/app.js
@@ -29,20 +29,33 @@ app.use(function(req, res, next) {
 /**********************
  * Example get method *
  **********************/
+const axios = require('axios')
 
 app.get(
   '/coins'
   , (req, res) => {
 
-    const coins = [
-      { name: 'Bitcoin', symbol: 'BTC', price_usd: "10000" },
-      { name: 'Ethereum', symbol: 'ETH', price_usd: "400" },
-      { name: 'Litecoin', symbol: 'LTC', price_usd: "150" }
-    ];
+    // Define base url
+    let apiUrl = `https://api.coinlore.com/api/tickers?start=0&limit=10`;
 
-    res.json({
-      coins
-    });
+    // Check if there are any query string parameters
+    // If so, reset the base url to include them
+    if (req.apiGateway && req.apiGateway.event.queryStringParameters) {
+      // Destructure the query string parameters... 
+      const { start = 0, limit = 10 } = req.apiGateway.event.queryStringParameters;
+      apiUrl = `https://api.coinlore.com/api/tickers/?start=${start}&limit=${limit}`;
+    }
+
+    // Call API and return response
+    axios.get(apiUrl)
+      .then(response => {
+        res.json({
+          coins: response.data.data
+        });
+      })
+      .catch(err => res.json({ 
+        error: err 
+      }));
   }
 );
 
